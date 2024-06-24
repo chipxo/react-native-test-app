ee6508e2ca5c17c63a2727c30749a9b1091d7f63
Serhii Chyipesh <chiypesh200059@gmail.com>
2024-06-24 21:17:03 +0300

    added tailwind and splash screen

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,36 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const primary = "#FA8A34";
+const background = "#D9D9D9";
+const secondaryGrey = "#636363";
+const secondaryRed = "#EE6363";
+const secondaryGreen = "#00A385";
+const tabIconDefault = "#606773";
+const primaryText = "#06070A";
+const secondaryText = "#858C94";
+const secondaryGreenText = "#009E81";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    primary: primary,
+    background: background,
+    secondaryGrey: secondaryGrey,
+    secondaryRed: secondaryRed,
+    secondaryGreen: secondaryGreen,
+    tabIconDefault: tabIconDefault,
+    tabIconSelected: primary,
+    primaryText: primaryText,
+    secondaryText: secondaryText,
+    secondaryGreenText: secondaryGreenText,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    primary: primary,
+    background: background,
+    secondaryGrey: secondaryGrey,
+    secondaryRed: secondaryRed,
+    secondaryGreen: secondaryGreen,
+    tabIconDefault: tabIconDefault,
+    tabIconSelected: primary,
+    primaryText: primaryText,
+    secondaryText: secondaryText,
+    secondaryGreenText: secondaryGreenText,
   },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,tsx,ts,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FA8A34",
+        background: "#D9D9D9",
+        "secondary-grey": "#636363",
+        "secondary-red": "#EE6363",
+        "secondary-green": "#00A385",
+        "tab-icon-default": "#606773",
+        "primary-text": "#06070A",
+        "secondary-text": "#858C94",
+        "secondary-green-text": "#009E81",
+      },
+    },
+  },
+  plugins: [],
+};
